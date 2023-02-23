4eaaf2ab584a9dad5e2e9848b2de26ba15deb8ec
hoahiep506 <hoahiep506@gmail.com>
2023-02-23 19:47:36 +0700

    fix/improve_css

--- src/page/layout/index.tsx
+++ src/page/layout/index.tsx
@@ -2,7 +2,7 @@ import { NavBar } from 'component';
 import { Outlet } from 'react-router-dom';
 const Layout = () => {
   return (
-    <div className='w-screen h-full  antialiased bg-green-base'>
+    <div className='w-full h-full  antialiased bg-green-base'>
       <NavBar />
       <Outlet />
     </div>
