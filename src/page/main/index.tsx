8e2f49dcb5e56c9f37d931552c6aa28a9ba6e052
hoahiep506 <hoahiep506@gmail.com>
2023-03-29 00:30:39 +0700

    feat: apply quote feature

--- src/page/main/index.tsx
+++ src/page/main/index.tsx
@@ -1,88 +1,96 @@
 import Quill from 'quill';
 import 'quill-mention';
 import 'quill/dist/quill.snow.css';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useId, useLayoutEffect, useRef, useState } from 'react';
+const Container = Quill.import('blots/container');
+const TextBlot = Quill.import('blots/text');
+const BlockEmbed = Quill.import('blots/block/embed');
 
-async function suggestPeople(searchTerm: string) {
-  const allPeople = [
-    {
-      id: 1,
-      value: 'Fredrik Sundqvist',
-    },
-    {
-      id: 2,
-      value: 'Patrik Sjölin',
-    },
-  ];
-  return allPeople.filter((person) => person.value.includes(searchTerm));
+class QuoteBlot extends BlockEmbed {
+  static blotName = 'quote';
+  static tagName = 'blockquote';
+  static className = 'quote';
+
+  static create(value: any) {
+    const node = super.create(value);
+    node.setAttribute('class', this.className);
+    node.innerHTML = value;
+    return node;
+  }
+
+  static value(node: { getAttribute: (arg0: string) => string }) {
+    return JSON.parse(node.getAttribute('data-quote'));
+  }
 }
+
+Quill.register(QuoteBlot);
 
 const MainPage = () => {
   const quillRef = useRef(null);
   const [editor, setEditor] = useState<Quill>();
+  const id = useId();
+  useLayoutEffect(() => {
+    const textEditor = document.getElementById(id);
+
+    return () => {};
+  }, []);
+
   useEffect(() => {
     if (quillRef.current) {
       const newEditor = new Quill(quillRef.current, {
-        theme: 'snow',
         modules: {
-          mention: {
-            allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
-            mentionDenotationChars: ['@', '#'],
-            source: async function (searchTerm: string, renderList: any) {
-              const matchedPeople = await suggestPeople(searchTerm);
-              renderList(matchedPeople);
-            },
-          },
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+            ['blockquote', 'code-block'],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+            [{ direction: 'rtl' }], // text direction
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+            [{ align: [] }],
+            ['image'],
+          ],
         },
-      });
-      newEditor.keyboard.addBinding(
-        { key: 'Escape' },
-        {
-          collapsed: true,
-          format: ['code-block'],
-        },
-        () => {
-          newEditor.insertText(newEditor.getLength() - 1, '\n');
-          newEditor.format('code-block', false);
-        }
-      );
-      newEditor.keyboard.addBinding(
-        { key: 'Backspace' },
-        {
-          collapsed: true,
-          format: ['code-block'],
-        },
-        () => {
-          newEditor.format('code-block', false);
-        }
-      );
-
-      newEditor.on('text-change', (delta) => {
-        const index = newEditor.getLength() - 1;
-        const one = newEditor.getContents(index - 1, 1)?.ops[0]?.insert;
-        const two = newEditor.getContents(index - 2, 1)?.ops[0]?.insert;
-        const three = newEditor.getContents(index - 3, 1)?.ops[0]?.insert;
-        const currentFormat = newEditor.getFormat();
-
-        if (
-          `${one}${two}${three}` === '```' &&
-          !currentFormat?.['code-block']
-        ) {
-          newEditor.deleteText(index - 3, 3);
-          console.log(newEditor.getLength());
-          if (newEditor.getLength() > 1) {
-            newEditor.insertText(newEditor.getLength() - 1, '\n');
-          }
-          newEditor.format('code-block', true);
-        }
+        theme: 'snow',
       });
       setEditor(newEditor);
     }
+    return () => {
+      quillRef.current = null;
+    };
   }, []);
+
+  const handleSubmit = () => {
+    console.log(editor?.root?.innerHTML);
+  };
+  const text = `<pre class="ql-syntax" spellcheck="false">asdasdasd
+  asdasdasd
+  asdas
+  </pre><blockquote>asdfsadfsadf</blockquote><ul><li>2</li><li>1</li></ul>`;
+
+  const handleQuote = () => {
+    navigator.clipboard.writeText(text);
+    const editorHTML = document.getElementById(id);
+    const temp = editorHTML?.querySelector('.ql-editor');
+    if (temp) {
+      // temp.innerHTML = `${temp?.innerHTML}<blockquote>${text}</blockquote>`;
+      if (editor) {
+        editor.insertEmbed(editor.getLength() - 1, 'quote', text);
+        editor.setSelection(editor.getLength(), 0);
+      }
+    }
+  };
 
   return (
     <div>
-      <div id='test' ref={quillRef}></div>
+      <div id={id} ref={quillRef}></div>
+      <button className='bg-red-400' onClick={handleSubmit}>
+        Submit
+      </button>
+
+      <p>${text}</p>
+
+      <button onClick={handleQuote}>Copy</button>
     </div>
   );
 };
